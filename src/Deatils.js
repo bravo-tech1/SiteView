eb6c06f3c904c1953d88aa448baf457259c4ecbb
NageebDarwish <nageeb9696youtub@gmail.com>
2022-09-28 17:14:23 +0300

    new update

--- src/Deatils.js
+++ src/Deatils.js
@@ -6,47 +6,32 @@ import Footer from "./components/Footer";
 import { useEffect } from "react";
 
 export default function Pack() {
-  const [data, setData] = useState([]);
-  const [images, setImages] = useState([]);
+  const [deatils, setDeatil] = useState([]);
   const [videos, setVideos] = useState([]);
 
   const id = Number(window.location.pathname.split("/").slice(-1)[0]);
+
   useEffect(() => {
-    fetch("https://test.emkanfinances.net/api/package/show")
+    fetch(`https://test.emkanfinances.net/api/detail/show`)
       .then((res) => res.json())
-      .then((dataRes) => setData(dataRes.filter((x) => x.id === id)));
+      .then((dataRes) => setDeatil(dataRes.filter((x) => x.package_id === id)));
   }, []);
   useEffect(() => {
-    fetch(`https://test.emkanfinances.net/api/package/showdetails/${id}`)
+    fetch(`https://test.emkanfinances.net/api/video/show`)
       .then((res) => res.json())
-      .then((dataRes) => setImages(dataRes[2]));
-  }, []);
-  useEffect(() => {
-    fetch(`https://test.emkanfinances.net/api/package/showdetails/${id}`)
-      .then((res) => res.json())
-      .then((dataRes) => setVideos(dataRes[1]));
+      .then((dataRes) => setVideos(dataRes));
   }, []);
 
-  const items = data.map((item) => (
+  const items = deatils.map((item) => (
     <div>
-      <h1 style={{ marginTop: "4rem" }}>{item.city_name_en}</h1>
-      <h1>{item.details_title_en}</h1>
-      <div className="WordStyle">{parse(item.details_text2_en)}</div>
-      <div className="row mt-4" style={{ rowGap: "10px" }}>
-        {images.map((itm) => (
-          <img src={itm.img} className="col-md-4" alt="countrys" />
-        ))}
-      </div>
-      <div
-        className="row"
-        style={{ marginTop: "2rem", marginBottom: "2rem", rowGap: "10px" }}
-      >
-        {videos.map((itm) => (
-          <video className="col-md-6" autoPlay muted>
-            <source src={`${itm.video}`} type="video/mp4" />
-          </video>
-        ))}
-      </div>
+      <div className="WordStyle">{parse(item.text_en)}</div>
+    </div>
+  ));
+  const videosI = videos.map((item) => (
+    <div className="col-md-4">
+      <video autoPlay muted>
+        <source src={`${item.video}`} type="video/mp4" />
+      </video>
     </div>
   ));
   return (
@@ -54,6 +39,7 @@ export default function Pack() {
       <Header />
       <div class="container" style={{ marginTop: "10%" }}>
         {items}
+        <div className="row">{videosI}</div>
       </div>
       <Footer />
     </>
--- src/DeatilsAr.js
+++ src/DeatilsAr.js
@@ -1,71 +1,47 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
-
-import HeaderAr from "./components/HeaderAr";
-
-import FooterAr from "./components/FooterAr";
+import parse from "html-react-parser";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import { useEffect } from "react";
 
 export default function Pack() {
-  const [data, setData] = useState([]);
-  const [images, setImages] = useState([]);
+  const [deatils, setDeatil] = useState([]);
   const [videos, setVideos] = useState([]);
 
   const id = Number(window.location.pathname.split("/").slice(-1)[0]);
+
   useEffect(() => {
-    fetch("https://test.emkanfinances.net/api/package/show")
+    fetch(`https://test.emkanfinances.net/api/detail/show`)
       .then((res) => res.json())
-      .then((dataRes) => setData(dataRes.filter((x) => x.id === id)));
+      .then((dataRes) => setDeatil(dataRes.filter((x) => x.package_id === id)));
   }, []);
   useEffect(() => {
-    fetch(`https://test.emkanfinances.net/api/package/showdetails/${id}`)
+    fetch(`https://test.emkanfinances.net/api/video/show`)
       .then((res) => res.json())
-      .then((dataRes) => setImages(dataRes[2]));
-  }, []);
-  useEffect(() => {
-    fetch(`https://test.emkanfinances.net/api/package/showdetails/${id}`)
-      .then((res) => res.json())
-      .then((dataRes) => setVideos(dataRes[1]));
+      .then((dataRes) => setVideos(dataRes));
   }, []);
 
-  const items = data.map((item) => (
-    <div dir="rtl" style={{marginBottom: '2rem'}}>
-      <h1 style={{marginTop: '4rem'}}>{item.city_name_ar}</h1>
-      <h1>{item.details_title_ar}</h1>
-      <h2 style={{ color: "black",
-    fontSize: '15px',
-    fontWeight: '200' }}>{item.details_text2_ar}</h2>
-      <div className="row mt-4" style={{rowGap: '10px'}}>
-        {images.map((itm) => (
-          <img src={itm.img} className="col-md-4" alt="countrys" />
-        ))}
-      </div>
-      <div className="row" style={{marginTop: '2rem', marginBottom: '2rem', rowGap: '10px'}}>
-    {videos.map((itm) => (
-       <video
-        className="col-md-6"
-       autoPlay
-       muted
-
-     >
-       <source
-         src={`${itm.video}`}
-         type="video/mp4"
-       />
-     </video>
-    ))}
-  </div>
+  const items = deatils.map((item) => (
+    <div>
+      <div className="WordStyle">{parse(item.text_ar)}</div>
     </div>
-   
+  ));
+  const videosI = videos.map((item) => (
+    <div className="col-md-4">
+      <video autoPlay muted>
+        <source src={`${item.video}`} type="video/mp4" />
+      </video>
+    </div>
   ));
   return (
     <>
-      <HeaderAr />
+      <Header />
       <div class="container" style={{ marginTop: "10%" }}>
         {items}
+        <div className="row">{videosI}</div>
       </div>
-      <FooterAr />
+      <Footer />
     </>
   );
 }
