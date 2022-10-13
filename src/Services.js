5706d666c5ad23c7eaf5acdcbb123a974e078586
NageebDarwish <bravotech0sa@gmail.com>
2022-10-13 09:58:12 -0900

    update

--- src/Services.js
+++ src/Services.js
@@ -134,7 +134,7 @@ export default function Services() {
         style={{
           width: "100%",
           height: "60vh",
-          margin: "4rem 0",
+          margin: " 0 0 4rem 0",
           overflow: "hidden",
         }}
       >
--- src/ServicesAr.js
+++ src/ServicesAr.js
@@ -94,7 +94,7 @@ export default function Services() {
                           backgroundRepeat: "no-repeat",
                           width: "100%",
                           backgroundSize: " cover",
-                          height: "11.25rem",
+                          height: "13.25rem",
                           position: "relative",
                         }}
                       ></div>
@@ -134,7 +134,7 @@ export default function Services() {
         style={{
           width: "100%",
           height: "60vh",
-          margin: "4rem 0",
+          margin: " 0 0 4rem 0",
           overflow: "hidden",
         }}
       >
