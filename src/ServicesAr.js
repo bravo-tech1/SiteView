7c29cf75a029466a9cf3936723ace81dda5f83ba
NageebDarwish <bravotech0sa@gmail.com>
2022-10-13 12:13:21 -0900

    update

--- src/ServicesAr.js
+++ src/ServicesAr.js
@@ -130,17 +130,27 @@ export default function Services() {
 
   return (
     <>
-      <div className="container text-center mb-3">
-        <h1>السفر حول العالم</h1>
-      </div>
       <div
         style={{
+          position: "relative",
           width: "100%",
           height: "60vh",
           margin: " 0 0 4rem 0",
           overflow: "hidden",
         }}
       >
+        <h1
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%,-50%)",
+            color: "white",
+            textAlign: "center",
+          }}
+        >
+          السفر حول العالم
+        </h1>
         <video
           width="100%"
           style={{ objectFit: "initial", height: "75vh" }}
